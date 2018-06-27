2b3cfe03c71af9276cc805c6838118d8ef28d8f1
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-26 22:04:13 -0600

    adding meals table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180626222929) do
+ActiveRecord::Schema.define(version: 20180627040238) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20180626222929) do
   create_table "foods", force: :cascade do |t|
     t.string "name"
     t.integer "calories"
+  end
+
+  create_table "meals", force: :cascade do |t|
+    t.string "name"
   end
 
 end
---
+++ spec/requests/api/vi/foods/meals_request_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+describe 'Meals API' do
+  it 'sends a all meals in database along with associated foods' do
+    get '/api/v1/meals'
+
+    expect(response).to be_success
+
+    meals = JSON.parse(response.body)
+    expect(meals.count).to eq(0)
+
+    meal_list = create_list(:meal, 3)
+
+    get '/api/v1/meals'
+
+    expect(response).to be_success
+
+    meals = JSON.parse(response.body)
+    expect(meals.count).to eq(meal_list.count)
+    expect(meals.first[:foods].count).to eq(meal_list.first.foods.count)
+  end
+end
