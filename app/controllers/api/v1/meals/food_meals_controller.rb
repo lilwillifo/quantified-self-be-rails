1ac1dac86ed944eaf4373ff5cfca21650f8d55e0
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-27 15:30:21 -0600

    refactor controller

--- app/controllers/api/v1/meals/food_meals_controller.rb
+++ app/controllers/api/v1/meals/food_meals_controller.rb
@@ -1,25 +1,27 @@
 class Api::V1::Meals::FoodMealsController < ActionController::API
   def create
-    meal = Meal.find(params[:id])
-    food = Food.find(params[:food_id])
     FoodMeal.create(food: food, meal: meal)
-    render json: message(food, meal), status: 201
+    message = "Successfully added #{food.name} to #{meal.name}"
+    render json: {"message": message}, status: 201
   rescue
     render status: 404, json: {}
   end
 
   def destroy
-    meal = Meal.find(params[:id])
-    food = Food.find(params[:food_id])
     join = meal.food_meals.find_by(food: params[:food_id])
     join.destroy
-    render json: {"message": "Successfully removed #{food.name} from #{meal.name}"}
+    message = "Successfully removed #{food.name} from #{meal.name}"
+    render json: {"message": message}
   rescue
     render status: 404, json: {}
   end
 
   private
-  def message(food, meal)
-    {"message": "Successfully added #{food.name} to #{meal.name}"}
+  def meal
+     Meal.find(params[:id])
+  end
+
+  def food
+    Food.find(params[:food_id])
   end
 end
