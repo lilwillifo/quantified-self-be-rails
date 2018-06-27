c081055d0f9543208e2f16db8381f46663bf7d6e
Margaret Williford <margaretwilliford4@gmail.com>
2018-06-27 13:52:51 -0600

    Fixing syntax in config for cors

--- config/application.rb
+++ config/application.rb
@@ -16,7 +16,7 @@ module QuantifiedSelfRails
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:any]
+        resource '*', headers: :any, methods: :any
       end
     end
 
